cdfd2d19c0a073f8e485c1b6d2dd157aa81ea20e
Rustam Shamsullin <sh.rustam@gmail.com>
2017-09-12 22:41:56 +0300

    Telegram appender

---
+++ src/log4net.Appender.Telegram/TelegramAppender.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using log4net.Core;
+using Telegram.Bot;
+
+namespace log4net.Appender.Telegram
+{
+    public class TelegramAppender : AppenderSkeleton
+    {
+        public string Token { get; set; }
+
+        public string ChatId { get; set; }
+
+        protected override void Append(LoggingEvent e)
+        {
+            if (string.IsNullOrEmpty(Token))
+            {
+                throw new ConfigurationErrorsException("PLease set the Token under TelegramAppender configuration section: <Token>...</Token>");
+            }
+
+            if (string.IsNullOrEmpty(ChatId))
+            {
+                throw new ConfigurationErrorsException("PLease set the ChatId under TelegramAppender configuration section: <ChatId>...</ChatId>");
+            }
+
+            var message = Layout == null ? e.RenderedMessage : RenderLoggingEvent(e);
+            var bot = new TelegramBotClient(Token);
+            bot.SendTextMessageAsync(ChatId, message);
+        }
+    }
+}
