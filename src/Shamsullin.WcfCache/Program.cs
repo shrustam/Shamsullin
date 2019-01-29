91e358665149f2e26964f5aa663accdfc52d6d29
Rustam Shamsullin <sh.rustam@gmail.com>
2019-01-29 20:05:57 +0300

    Shamsullin.WcfCache

---
+++ src/Shamsullin.WcfCache/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using Shamsullin.Wcf;
+
+namespace Shamsullin.WcfCache
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Install Certificate
+            var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadOnly);
+            var certs = store.Certificates.Cast<X509Certificate2>();
+            var cert = certs.First(x => x.HasPrivateKey);
+
+            var appid = GetAppId();
+            var proc = new Process();
+            proc.StartInfo.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.SystemX86), "netsh.exe");
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.Arguments = $"http add sslcert ipport=127.0.0.1:443 certhash={cert.Thumbprint} appid={{{appid}}}";
+            proc.Start();
+            proc.WaitForExit();
+
+            // Start WCF
+            var wcf = new ServiceHost(typeof(WcfCacheService), new Uri("https://127.0.0.1/wcfcache"));
+            var httpsBinding = new WebHttpBinding(WebHttpSecurityMode.Transport);
+            wcf.AddServiceEndpoint(typeof(WcfCacheService), httpsBinding, "").EndpointBehaviors.Add(new WcfRestBehavior());
+            wcf.Open();
+
+            // Keep console open
+            while (true) Console.ReadKey();
+        }
+
+        private static string GetAppId()
+        {
+            var assembly = typeof(Program).Assembly;
+            var attribute = (GuidAttribute)assembly.GetCustomAttributes(typeof(GuidAttribute), true)[0];
+            var result = attribute.Value;
+            return result;
+        }
+    }
+}
---
+++ src/Shamsullin.WcfCache/WcfCacheService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Web.Services;
+using Shamsullin.Wcf;
+
+namespace Shamsullin.WcfCache
+{
+    [ServiceContract]
+    [WcfRestErrorHandler]
+    public class WcfCacheService : WebService
+    {
+        private static readonly Hashtable Hashtable = new Hashtable();
+
+        public class Record
+        {
+            public readonly DateTime Timestamp = DateTime.Now;
+
+            public string Key;
+
+            public byte[] Value;
+
+            public TimeSpan? Expiry;
+        }
+
+        [WebInvoke(Method = "POST")]
+        public byte[] Get(string key)
+        {
+            var result = Hashtable[key] as Record;
+            if (result?.Expiry != null && result.Timestamp+result.Expiry > DateTime.Now)
+            {
+                Hashtable[key] = null;
+                return null;
+            }
+
+            return result?.Value;
+        }
+
+        [WebInvoke(Method = "POST")]
+        public void Set(Record record)
+        {
+            Hashtable[record.Key] = record;
+        }
+    }
+}
