efd0fd3995bf020817de5162e241b525528d6fa0
Rustam Shamsullin <sh.rustam@gmail.com>
2019-02-01 08:17:26 +0300

    small fixes

--- nuget/Shamsullin.WinService/content/WinService.cs
+++ nuget/Shamsullin.WinService/content/WinService.cs
@@ -119,10 +119,7 @@ namespace Shamsullin
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing) components?.Dispose();
             base.Dispose(disposing);
         }
 
