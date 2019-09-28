f4e044d7c29856fbe2ab24681ff1949436b1a235
Rustam Shamsullin <sh.rustam@gmail.com>
2019-09-28 12:27:15 +0300

    nuget packages adjusted

--- nuget/Shamsullin.Win/pack.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" "..\..\Shamsullin.sln" /t:Build /p:Configuration=Release
-..\nuget pack manifest.nuspec
--- nuget/log4net.Appender.Telegram/pack.bat
+++ nuget/log4net.Appender.Telegram/pack.bat
@@ -1,2 +1,3 @@
+del /q *.nupkg
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild" "..\..\Shamsullin.sln" /t:Build /p:Configuration=Release
 ..\nuget pack manifest.nuspec
